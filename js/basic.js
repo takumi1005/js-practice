a88d56b429e14921f89ed7d61db7245d2357d914
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-09 18:24:14 +0900

    数値の計算

--- js/basic.js
+++ js/basic.js
@@ -1,8 +1,27 @@
 "use strict";
 
+/*
 console.log("Hello World");
 
 // \nで改行\tでタブを作成できる
 console.log("hel\nlo wor\tld");
 // 文字列の連結
 console.log("hello" + "world");
+*/
+
+// 数値の計算
+// 足し算
+console.log(10 + 3);
+// 引き算
+console.log(10 - 3);
+// 掛け算
+console.log(10 * 3);
+// 割り算
+console.log(10 / 3);
+// 余り
+console.log(10 % 3);
+// べき乗
+console.log(10 ** 3); // 1000
+// 計算の優先度
+console.log(2 + 10 * 3); // 32
+console.log((2 + 10) * 3); // 36
