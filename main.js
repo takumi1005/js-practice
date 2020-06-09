27634e7dc2f38aeb3204ff7d832c659dea5ffceb
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-09 14:56:42 +0900

    cssのクラスを操作

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 document.getElementById("target").addEventListener("click", () => {
-  document.getElementById("target").style.background = "pink";
-  document.getElementById("target").style.borderRadius = "50%";
+  // document.getElementById("target").style.background = "pink";
+  // document.getElementById("target").style.borderRadius = "50%";
+  document.getElementById("target").classList.add("circle");
 });
