8708e2f570489fc39e4df6b7eed52d50bc32cd30
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-09 15:35:56 +0900

    divを生成

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 "use strict";
 
-const target1 = document.getElementById("target1");
-const target2 = document.getElementById("target2");
-const target3 = document.getElementById("target3");
+const div = document.createElement("div");
+div.classList.add("box");
 
-target1.addEventListener("click", () => {
-  target1.classList.toggle("circle");
+div.addEventListener("click", () => {
+  div.classList.toggle("circle");
 });
-target2.addEventListener("click", () => {
-  target2.classList.toggle("circle");
-});
-target3.addEventListener("click", () => {
-  target3.classList.toggle("circle");
-});
+
+document.body.appendChild(div);
