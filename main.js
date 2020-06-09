37a80f625b551e091968b8e073eaa2cdb7601882
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-09 15:00:27 +0900

    定数を使う

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 
-document.getElementById("target").addEventListener("click", () => {
-  // document.getElementById("target").style.background = "pink";
-  // document.getElementById("target").style.borderRadius = "50%";
-  document.getElementById("target").classList.add("circle");
+const target = document.getElementById("target");
+
+target.addEventListener("click", () => {
+  target.classList.toggle("circle");
 });
