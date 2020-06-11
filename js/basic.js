0913774d2b3d43d8b1c9cdfe07f20b8c11aec3bb
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-11 14:30:32 +0900

    アロー関数

--- js/basic.js
+++ js/basic.js
@@ -210,10 +210,22 @@ function sum(a, b, c) {
 // sum(3, 4, 5);
 const total = sum(1, 2, 3) + sum(3, 4, 5);
 console.log(total);
-*/
+
 // 関数式
 const sum = function (a, b, c) {
   return a + b + c;
 };
 const total = sum(1, 2, 3) + sum(3, 4, 5);
 console.log(total);
+*/
+// アロー関数
+// const sum = (a, b, c) => a + b + c;
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total);
+
+// const double = function(a){
+//   return a * 2;
+// };
+// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+const double = (a) => a * 2;
+console.log(double(12));
