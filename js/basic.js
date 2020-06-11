32a3d11b4f4030be9526506d3c49dffbc554efce
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-11 14:22:16 +0900

    関数式

--- js/basic.js
+++ js/basic.js
@@ -200,7 +200,7 @@ showAd();
 console.log("Steve is great!");
 console.log("Richard is great!");
 showAd("Footer Ad");
-*/
+
 // return
 function sum(a, b, c) {
   // console.log(a + b + c);
@@ -208,5 +208,12 @@ function sum(a, b, c) {
 }
 // sum(1, 2, 3);
 // sum(3, 4, 5);
+const total = sum(1, 2, 3) + sum(3, 4, 5);
+console.log(total);
+*/
+// 関数式
+const sum = function (a, b, c) {
+  return a + b + c;
+};
 const total = sum(1, 2, 3) + sum(3, 4, 5);
 console.log(total);
