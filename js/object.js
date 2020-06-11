639ac42867a1bc071e3abf513dc6c880ff2a5668
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-11 16:48:56 +0900

    分割代入

--- js/object.js
+++ js/object.js
@@ -51,7 +51,7 @@
     console.log(`Score ${i}: ${scores[i]}`);
   }
 }
-*/
+
 // スプレッド構文
 {
   const otherScores = [10, 20];
@@ -62,4 +62,25 @@
     console.log(a + b);
   }
   sum(...otherScores);
+}
+*/
+// 分割代入
+{
+  const scores = [80, 90, 40, 70];
+
+  // const [a, b, c, d] = scores;
+  // console.log(a);
+  // console.log(b);
+  // console.log(c);
+  // console.log(d);
+  const [a, b, ...others] = scores;
+  console.log(a);
+  console.log(b);
+  console.log(others);
+
+  let x = 30;
+  let y = 70;
+  [x, y] = [y, x]; //xとyの値を入れ替え出来る
+  console.log(x);
+  console.log(y);
 }
