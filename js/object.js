01a9b91ac57bec3e2d0d974a39e7b7351b76d8cf
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-11 15:16:07 +0900

    配列とループ処理

--- js/object.js
+++ js/object.js
@@ -9,7 +9,7 @@
   const scores = [80, 90, 40];
   console.log(scores);
 }
-*/
+
 // 配列にアクセス
 {
   const scores = [80, 90, 40];
@@ -21,4 +21,14 @@
   // scores = 10;
 
   console.log(scores.length);
+}
+*/
+// 配列とループ処理
+{
+  const scores = [80, 90, 40, 70];
+
+  // for (let i = 0; i < 3; i++) {
+  // 下記のようにすれば配列の中身が増えても対応できる
+  for (let i = 0; i < scores.length; i++)
+    console.log(`Score ${i}: ${scores[i]}`);
 }
