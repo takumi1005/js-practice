bf5ad2c3939d50d393ab75107eb1b4116c71e62d
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-11 11:37:22 +0900

    条件分岐

--- js/basic.js
+++ js/basic.js
@@ -62,7 +62,7 @@ console.log(typeof 5);
 console.log(typeof true);
 console.log(typeof undefined);
 console.log(typeof null);
-*/
+
 
 // 数字からなる文字列
 // console.log('5' * 3);  掛け算される
@@ -71,3 +71,28 @@ console.log(typeof null);
 // console.log(parseInt('5', 10) + 3);  数値型に変換され8と出力される
 // console.log(parseInt('hello', 10));  NaNと出力される
 
+// 比較演算子
+const price = 1200;
+// console.log(price > 1000); // true
+// console.log(price < 1000); // false
+// console.log(price >= 1000); // true
+// console.log(price <= 1000); // false
+// console.log(price === 1000); // false
+// console.log(price !== 1000); // true
+
+// false <- 0, null, undefined, '', false
+// true <- それ以外
+console.log(Boolean(0));
+console.log(Boolean("hello"));
+*/
+
+// 条件分岐 if
+const score = 40;
+
+if (score >= 80) {
+  console.log("Great!");
+} else if (score >= 60) {
+  console.log("Good.");
+} else {
+  console.log("OK...");
+}
