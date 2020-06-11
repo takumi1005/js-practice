84ceb9da9fd74e9196311936f8453e59a67f68af
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-11 15:02:05 +0900

    配列にアクセス

--- js/object.js
+++ js/object.js
@@ -1,4 +1,6 @@
+"use strict";
 // 配列を作成
+/*
 {
   // const score1 = 80;
   // const score2 = 90;
@@ -6,4 +8,17 @@
   // ↓↓↓↓↓↓↓↓↓
   const scores = [80, 90, 40];
   console.log(scores);
+}
+*/
+// 配列にアクセス
+{
+  const scores = [80, 90, 40];
+  // console.log(scores[1]);
+
+  // scores[2] = 44;
+  // console.log(scores);
+
+  // scores = 10;
+
+  console.log(scores.length);
 }
