990b9d8aca11f302d2d068cde7e10000df9a8266
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-12 11:06:23 +0900

    filterを使う

--- js/object.js
+++ js/object.js
@@ -92,7 +92,7 @@
     console.log(`Score ${index}: ${score}`);
   });
 }
-*/
+
 // mapを使う
 {
   const prices = [180, 190, 200];
@@ -103,4 +103,18 @@
   // 省略すると↓
   const updatePrices = prices.map((price) => price + 20);
   console.log(updatePrices);
+}
+*/
+// filterを使う
+{
+  const numbers = [1, 4, 7, 8, 10];
+  // const evenNumbers = number.filter(number => {
+  //   if (number % 2 === 0){
+  //     return true;
+  //   }else{
+  //     return false;
+  //   }
+  // });
+  const evenNumbers = numbers.filter((number) => number % 2 === 0);
+  console.log(evenNumbers);
 }
