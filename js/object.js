89c249c8cc1dd9ebb63025001ebd26d3f8a8c98e
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-15 15:58:50 +0900

    静的メソッド

--- js/object.js
+++ js/object.js
@@ -387,7 +387,7 @@
   posts[0].show();
   posts[1].show();
 }
-*/
+
 // カプセル化
 {
   class Post {
@@ -420,4 +420,38 @@
 
   // posts[0].show();
   // posts[1].show();
+}
+*/
+// 静的メソッド
+{
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+
+    // 静的メソッド
+    // thisは使えない
+    static showInfo() {
+      console.log("Post class version 1.0");
+    }
+  }
+
+  const posts = [
+    new Post("JavaScriptの勉強中…"),
+    new Post("プログラミング楽しい！"),
+  ];
+
+  // posts[0].like();
+
+  Post.showInfo();
 }
