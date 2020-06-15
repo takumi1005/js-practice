744b85e68c7a45a293f5e26dcd98b8a2b2755cc4
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-15 17:26:50 +0900

    要素の属性を操作

--- js/dom.js
+++ js/dom.js
@@ -12,10 +12,21 @@
   }
   setTimeout(update, 1000);
 }
-*/
+
 // addEventListenerを使う
 {
   document.querySelector("button").addEventListener("click", () => {
     document.getElementById("target").textContent = "Changed!";
+  });
+}
+*/
+// 要素の属性を操作
+{
+  document.querySelector("button").addEventListener("click", () => {
+    const targetNode = document.getElementById("target");
+    targetNode.textContent = "Changed!";
+    targetNode.title = "This is title!";
+    targetNode.style.color = "red";
+    targetNode.style.backgroundColor = "skyblue";
   });
 }
