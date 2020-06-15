d1d394087b66e86f845117ddfcf9950838646d61
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-15 15:32:34 +0900

    クラスの作成

--- js/object.js
+++ js/object.js
@@ -340,7 +340,7 @@
   }
   console.log("Finish!");
 }
-*/
+
 // メソッドを使う
 {
   const posts = [
@@ -365,6 +365,25 @@
   ];
 
   // show(posts[0]);
+  posts[0].show();
+  posts[1].show();
+}
+*/
+// クラスの作成
+{
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+    show() {
+      console.log(`${this.text} - ${this.likeCount}いいね`);
+    }
+  }
+  const posts = [
+    new Post("JavaScriptの勉強中…"),
+    new Post("プログラミング楽しい"),
+  ];
   posts[0].show();
   posts[1].show();
 }
