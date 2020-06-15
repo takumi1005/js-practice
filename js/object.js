b2e734ade0f490d0f89e1d63e1f8d05248ec153c
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-15 16:04:57 +0900

    クラスの継承

--- js/object.js
+++ js/object.js
@@ -421,7 +421,7 @@
   // posts[0].show();
   // posts[1].show();
 }
-*/
+
 // 静的メソッド
 {
   class Post {
@@ -454,4 +454,46 @@
   // posts[0].like();
 
   Post.showInfo();
+}
+*/
+// クラスの継承
+{
+  class Post {
+    // 親クラス
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+  }
+
+  class SponsoredPost extends Post {
+    // 子クラス
+    constructor(text, sponsor) {
+      super(text);
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      super.show();
+      console.log(`... sponsored by ${this.sponsor}`);
+    }
+  }
+
+  const posts = [
+    new Post("JavaScriptの勉強中…"),
+    new Post("プログラミング楽しい！"),
+    new SponsoredPost("3分動画でマスターしよう", "dotinstall"),
+  ];
+
+  posts[2].show();
+  posts[2].like();
 }
