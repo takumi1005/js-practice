905b045f44a9fa319787780f13716803ca34700f
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-15 15:38:13 +0900

    カプセル化

--- js/object.js
+++ js/object.js
@@ -368,7 +368,7 @@
   posts[0].show();
   posts[1].show();
 }
-*/
+
 // クラスの作成
 {
   class Post {
@@ -386,4 +386,38 @@
   ];
   posts[0].show();
   posts[1].show();
+}
+*/
+// カプセル化
+{
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+    //ここで定義することによって修正がしやすい
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+  }
+
+  const posts = [
+    new Post("JavaScriptの勉強中…"),
+    new Post("プログラミング楽しい！"),
+  ];
+
+  posts[0].like();
+  // 冗長になってしまう
+  // posts[0].likeCount++;
+  // posts[0].likeCount++;
+  // posts[0].likeCount++;
+  // posts[0].likeCount++;
+
+  // posts[0].show();
+  // posts[1].show();
 }
