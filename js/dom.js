19f308e4c14f1cb2359f75af6f05281e4967edbe
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-16 15:20:43 +0900

    ラジオボタンを操作

--- js/dom.js
+++ js/dom.js
@@ -106,13 +106,29 @@
     text.focus();
   });
 }
-*/
+
 // セレクトボックスを操作
 {
   document.querySelector("button").addEventListener("click", () => {
     const li = document.createElement("li");
     const color = document.querySelector("select");
     li.textContent = `${color.value} - ${color.selectedIndex}`;
+    document.querySelector("ul").appendChild(li);
+  });
+}
+*/
+// ラジオボタンを操作
+{
+  document.querySelector("button").addEventListener("click", () => {
+    const colors = document.querySelectorAll("input");
+    let selectedColor;
+    colors.forEach((color) => {
+      if (color.checked === true) {
+        selectedColor = color.value;
+      }
+    });
+    const li = document.createElement("li");
+    li.textContent = selectedColor;
     document.querySelector("ul").appendChild(li);
   });
 }
