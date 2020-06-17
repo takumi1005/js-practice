aefa48d5a11929468ea8a6fa3d7a2a45cc05e9c4
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-17 11:50:49 +0900

    処理を関数にまとめる

--- js/bingo.js
+++ js/bingo.js
@@ -1,21 +1,30 @@
 "use strict";
 
 {
-  // const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  // for文を使って簡単に
-  const source = [];
-  for (let i = 0; i < 15; i++) {
-    source[i] = i + 1;
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      // 1 ... 15
+      // source[i] = i + 1 + 15 * 0;
+      // 16 ... 30
+      // source[i] = i + 1 + 15 * 1;
+      // 31 ... 45
+      // source[i] = i + 1 + 15 * 2;
+      source[i] = i + 1 + 15 * col;
+    }
+
+    const b = [];
+    for (let i = 0; i < 5; i++) {
+      b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+
+    console.log(b);
   }
-  const b = [];
-  // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  //こちらも簡単に
-  for (let i = 0; i < 5; i++) {
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  }
-  console.log(b);
+
+  const columns = [];
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4);
 }
