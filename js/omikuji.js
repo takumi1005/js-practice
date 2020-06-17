a308dc3880044071ade2f22c8803ebb3920cf3d5
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-17 11:11:32 +0900

    確率を操作する

--- js/omikuji.js
+++ js/omikuji.js
@@ -3,11 +3,13 @@
 {
   const btn = document.getElementById("btn");
   btn.addEventListener("click", () => {
-    const results = ["大吉", "中吉", "凶", "末吉"];
+    // 配列
+    // const results = ["大吉", "中吉", "凶", "末吉"];
     // const n = Math.floor(Math.random() * results.length);
     // btn.textContent = results[n];
     // 省略↓
-    btn.textContent = results[Math.floor(Math.random() * results.length)];
+    // btn.textContent = results[Math.floor(Math.random() * results.length)];
+    // スイッチ文
     // switch (n) {
     //   case 0:
     //     btn.textContent = "大吉";
@@ -19,5 +21,14 @@
     //     btn.textContent = "凶";
     //     break;
     // }
+    // 確率を操作
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = "大吉"; //5%の確率
+    } else if (n < 0.2) {
+      btn.textContent = "中吉"; //15%
+    } else {
+      btn.textContent = "凶"; //80%
+    }
   });
 }
