2f20045d61959043db709595849bc6b2871a9858
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-17 16:48:00 +0900

    ストップ・リセット機能

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -7,6 +7,7 @@
   const reset = document.getElementById('reset');
 
   let startTime;
+  let timeoutId;
 
   function countUp() {
     const d  = new Date(Date.now() - startTime);
@@ -14,7 +15,7 @@
     const s  = String(d.getSeconds()).padStart(2, '0');
     const ms = String(d.getMilliseconds()).padStart(3, '0');
     timer.textContent = `${m}:${s}.${ms}`;
-    setTimeout(() => {
+    timeoutId = setTimeout(() => {
       countUp();
     }, 10);
   }
@@ -22,5 +23,11 @@
   start.addEventListener('click', () => {
     startTime = Date.now();
     countUp();
+  });
+  stop.addEventListener('click', () => {
+    clearTimeout(timeoutId);
+  });
+  reset.addEventListener('click', () => {
+    timer.textContent = '00:00.000';
   });
 }
