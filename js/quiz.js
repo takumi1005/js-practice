f28ae6ef0bfec48f78b237ea99bdb0b4eb07cfb8
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-18 16:25:03 +0900

    クイズデータの作成

---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+'use srtict';
+
+{
+  const question = document.getElementById('question');
+  const choises = document.getElementById('choises');
+  const btn = document.getElementById('question');
+
+  const quizSet = [
+    {q: 'What is A?', c: ['A0', 'A1', 'A2']},
+    {q: 'What is B?', c: ['B0', 'B1', 'B2']},
+    {q: 'What is C?', c: ['C0', 'C1', 'C2']},
+  ];
+  let currentNum = 0;
+}
