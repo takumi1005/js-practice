adf5b69cc8b3e4bdac6548475efc5498fc41d739
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-23 15:05:37 +0900

    四角を描画する

--- js/canvas.js
+++ js/canvas.js
@@ -2,8 +2,17 @@
 
 {
   function draw() {
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext === 'undefined') {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
 
+    // ctx.fillRect(x, y, width, height);
+    // 四角を描画(塗り潰し)
+    // ctx.fillRect(50, 50, 50, 50)
+    // 四角を描画(枠のみ)
+    ctx.strokeRect(50, 50, 50, 50)
   }
-
   draw();
 }
