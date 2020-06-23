b55dcc5d7c677571bf03e90f3d1a25133aebccab
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-23 12:39:13 +0900

    menuにイベントを設定

---
+++ js/tabmenu.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', e => {
+      e.preventDefault();
+
+      item.classList.add('active');
+    });
+  });
+}
