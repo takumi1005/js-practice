6fcb77a5557abbd57c357dfd89ccf8d42f19a2bd
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-06-23 15:55:29 +0900

    複数の図形の描画

--- js/canvas.js
+++ js/canvas.js
@@ -9,12 +9,15 @@
     const ctx = canvas.getContext('2d');
 
     ctx.fillStyle = 'pink';
-    ctx.fillRect(50, 50, 50, 50);
     ctx.strokeStyle = '#f00';
-    ctx.lineWidth = 8;
-    // ctx.lineJoin = 'round';
-    ctx.lineJoin = 'bevel';
+    ctx.fillRect(50, 50, 50, 50);
     ctx.strokeRect(50, 50, 50, 50);
+
+    ctx.fillStyle = 'skyblue';
+    ctx.strokeStyle = '#00b';
+
+    ctx.fillRect(70, 70, 50, 50);
+    ctx.strokeRect(70, 70, 50, 50);
   }
   draw();
 }
