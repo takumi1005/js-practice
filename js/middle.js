c238a6ba55bb7153a6abb8ef86acc579bab9c2cf
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-07-13 16:51:34 +0900

    js_pra_m3

--- js/middle.js
+++ js/middle.js
@@ -3,8 +3,16 @@
 $(function() {
   alert('jqueryでアラート表示');
 });
-*/
+
 // 要素の横幅を取得
 $(function() {
   alert("横幅は" + $('.box').width() + "です");
+});
+*/
+// カラーピッカーで取得した色を反映させる
+$(function() {
+  $('#color-change').on("change", function() {
+    let select_color = $(this).val();
+    $('.title').css('color', select_color);
+  });
 });
